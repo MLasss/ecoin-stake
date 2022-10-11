7319203a3515731994d9dcffc3920f0f0e451526
unknown <MarcusLaska@protonmail.com>
2022-10-11 18:24:14 +0200

    point to mainnet

--- src/lib/alchemyClient.js
+++ src/lib/alchemyClient.js
@@ -2,7 +2,7 @@ import { Network, Alchemy } from "alchemy-sdk";
 
 const settings = {
     apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-    network: Network.MATIC_MUMBAI,
+    network: Network.MATIC_MAINNET,
 };
 
 const alchemy = new Alchemy(settings);
