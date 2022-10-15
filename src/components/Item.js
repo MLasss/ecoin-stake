f2fd23bd405f38904d5f6b7781c4aba2d87e7f83
unknown <MarcusLaska@protonmail.com>
2022-10-15 11:11:30 +0200

    bug fix for img first load

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,40 +3,40 @@ import { useNavigate } from "react-router-dom";
 import LoadingImage from "../assets/images/loading.gif";
 import "../assets/css/Item.css";
 
-
 function Item( {token} ) {
   const [ metadata, setMetdata ] = useState(null);
   const navigate = useNavigate();
 
   useEffect(()=> {
+    setMetdata(null);
     if (token?.rawMetadata){
       let meta = token.rawMetadata;
       if (meta?.image){
-        meta.image = meta.image.replace("ipfs://", "https://gateway.ipfs.io/ipfs/");
+        meta.image = meta.image.replace("ipfs://", process.env.REACT_APP_IPFS_GATEWAY);
       }
-      setMetdata(meta)
+      setTimeout(function() { setMetdata(meta) }, 200);
     }
   },[token])
 
   function navigateToItem(){
     navigate(`/items/${token.tokenId}`);
   }
+
   return (
     <div className="card my-3 item-card" onClick={navigateToItem}>
-    <div className="card-header" style={{backgroundColor: "#"+metadata?.background_color}}>
+    <div className="card-header" style={{backgroundColor: "#" + metadata?.background_color}}>
       <img 
         className="card-img-top" 
         onError={({ currentTarget }) => {
           currentTarget.src=LoadingImage;
-          setTimeout(function() { currentTarget.src=metadata?.image; }, 200);
+          setTimeout(function() { currentTarget.src = metadata?.image; }, 200);
         }}
-        src={metadata?.name} 
+        src={metadata != null ? metadata?.image : LoadingImage} 
         alt="..."
       />
     </div>
-    
     <div className="card-body">
-      <h5 className="card-title">{metadata?.name}</h5>
+      <h5 className="card-title">{metadata != null ? metadata?.name : "Loading..."}</h5>
     </div>
   </div>
   );
